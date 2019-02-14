fa73e5fe42277928c900d90054c59ba77c4c5244
ceremcem <ceremcem@fc2.lxc>
2019-02-14 16:21:40 -0500

    improved documentation

--- config.sh
+++ config.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 FREECAD=/opt/FreeCAD
 
-DEBUG_CPU=$(nproc) 	# Full throttle
-RELEASE_CPU=99  	# Full throttle + install this one
+# 0 -> do not compile
+# $(nproc) -> full throttle
+# more cpu means "install this one"
+DEBUG_CPU=0
+RELEASE_CPU=99
 
 Asm3_Commit="master"
