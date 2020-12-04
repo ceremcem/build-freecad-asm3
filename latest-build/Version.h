d70bb2982623f6c41dbfdf8ff54112a53f2a2455
Cerem Cem ASLAN <cem@aktos.io>
2020-12-04 10:19:17 +0000

    bump last build version

--- latest-build/Version.h
+++ latest-build/Version.h
@@ -4,11 +4,11 @@
 #define FCVersionMinor "19"
 #define FCVersionName  "Vulcan"
 // test: $Format:Hash (%H), Date: %ci$
-#define FCRevision      "25952 (Git)"      //Highest committed revision number
-#define FCRevisionDate  "2020/12/03 01:08:38"     //Date of highest committed revision
+#define FCRevision      "25968 (Git)"      //Highest committed revision number
+#define FCRevisionDate  "2020/12/04 08:44:55"     //Date of highest committed revision
 #define FCRepositoryURL "git://github.com/realthunder/FreeCAD.git LinkStage3"      //Repository URL of the working copy
 
 // Git relevant stuff
-#define FCRepositoryHash   "ebaa289b35f040a0b3f5f183474760adacbec03c"
+#define FCRepositoryHash   "e8e5fefd360938dff9648a102c5d46442780d797"
 #define FCRepositoryBranch "LinkStage3"
 
