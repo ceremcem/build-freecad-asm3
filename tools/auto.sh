ceremcem/build-freecad-asm3

commit 9bd1446f1a277b75d6776586e4336ad4c4d702a0

fixed requirement for running tools/auto.sh twice.

tools/auto.sh
@@ -86,42 +86,34 @@ is_on_btrfs "$LXC_PATH" && bdev="-B btrfs" || bdev=""
 
 CHROOT="$_sdir/run-in-chroot.sh -n $container_name"
 
-if [[ ! -d $LXC_PATH/$container_name ]]; then 
-    set -x 
-    apt-get install debian-keyring debian-archive-keyring
-    lxc-create -n $container_name -t debian $bdev -- -r buster # might not work: --packages 
-    set +x
-    if lxc-start -n $container_name; then 
-        echo "$container_name successfully created."
-        while :; do
-            timeout 3 lxc-stop -n $container_name && break
-            echo "Retrying stopping $container_name"
-            sleep 2
-        done
-    else
-        die "Couldn't start $container_name."
-    fi
-
-    # install basic packages
-    packages="nano sudo git"   # tmux
-    $support_ssh_x && packages="$packages xbase-clients"
-
-    password=$user
-    $CHROOT -- "apt-get update && apt-get install -y $packages; \
-        grep $user /etc/passwd -q || { \
-            echo 'adding user $user'; \
-            useradd -m $user; \
-            usermod -a -G sudo $user; \
-            echo "$user:$password" | chpasswd; \
-        }"
-
-    echo "Exiting for a workaround, see: https://unix.stackexchange.com/q/627262/65781"
-    echo "Manually restart this script once more."
-    exit 2
-else 
-    echo "Container $container_name seems to be already created."
+set -x 
+apt-get install debian-keyring debian-archive-keyring
+lxc-create -n $container_name -t debian $bdev -- -r buster # might not work: --packages 
+set +x
+if lxc-start -n $container_name; then 
+    echo "$container_name successfully created."
+    while :; do
+       timeout 3 lxc-stop -n $container_name && break
+       echo "Retrying stopping $container_name"
+       sleep 2
+    done
+else
+    die "Couldn't start $container_name."
 fi
-# LXC container is created. 
+
+# install basic packages
+packages="nano sudo git"   # tmux
+$support_ssh_x && packages="$packages xbase-clients"
+
+password=$user
+$CHROOT -- "apt-get update && apt-get install -y $packages; \
+    grep $user /etc/passwd -q || { \
+        echo 'adding user $user'; \
+        useradd -m $user; \
+        usermod -a -G sudo $user; \
+        echo "$user:$password" | chpasswd; \
+    }"
+# LXC container is created.
 
 builder_scripts="$LXC_PATH/$container_name/rootfs/home/$user/$(basename $(dirname $_sdir))"
 if [[ ! -d "$builder_scripts" ]]; then

tools/run-in-chroot.sh
@@ -129,7 +129,7 @@ fi
 
 if [[ ${args[@]} || -n $user ]]; then
     tmp_file=/tmp/cmd.sh
-    cmd="--rcfile $tmp_file"
+    cmd="-f $tmp_file"
     echo '#/bin/bash' > $rootfs/$tmp_file
     echo "cd" >> $rootfs/$tmp_file
     echo "[[ -f .bashrc ]] && . .bashrc" >> $rootfs/$tmp_file

commit 49293bf8fc95faaf669ea3ed930682c456fc04a9

fixed interactive chroot command issue which introduced in 9bd1446

tools/auto.sh
@@ -101,7 +101,7 @@ is_on_btrfs(){
 
 is_on_btrfs "$LXC_PATH" && bdev="-B btrfs" || bdev=""
 
-CHROOT="$_sdir/run-in-chroot.sh -n $container_name"
+CHROOT="$_sdir/run-in-chroot.sh -n $container_name --unattended"
 
 set -x 
 apt-get install debian-keyring debian-archive-keyring

tools/run-in-chroot.sh
@@ -41,6 +41,7 @@ user=root
 verbose=false
 cmd=
 _umount=false
+unattended=false
 # ---------------------------
 args_backup=("$@")
 args=()
@@ -64,6 +65,9 @@ while [ $# -gt 0 ]; do
             ;;
         --umount)
             _umount=true
+            ;;
+        --unattended)
+            unattended=true
             ;;
         --) shift
             args=("$@")
@@ -129,7 +133,11 @@ fi
 
 if [[ ${args[@]} || -n $user ]]; then
     tmp_file=/tmp/cmd.sh
-    cmd="-f $tmp_file"
+    if $unattended; then
+        cmd="-f $tmp_file"
+    else
+        cmd="--rcfile $tmp_file"
+    fi
     echo '#/bin/bash' > $rootfs/$tmp_file
     echo "cd" >> $rootfs/$tmp_file
     echo "[[ -f .bashrc ]] && . .bashrc" >> $rootfs/$tmp_file