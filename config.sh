21d21de9899379de3cbc41496720d01a4479ec81
Cerem Cem ASLAN <cem@aktos.io>
2021-11-24 13:41:29 +0000

    added comments

--- config.sh
+++ config.sh
@@ -1,7 +1,24 @@
-build_type="Release"	# Options: "Release" or "Debug"
+# Build type
+# Options: "Release" or "Debug"
+build_type="Release"
+
+# Directory for the output binary
 build_dir="../fc-build/$build_type"
+
+# FreeCAD git clone location:
 src="$HOME/FreeCAD"
+
+# Number of CPU to use while compiling
 CPU=$(nproc)
-branch="LinkStage3"
+
+# Configure branch
+# latest: LinkDaily
+# stable: LinkStage3
+branch="LinkDaily"
+
+# If you want to compile against a
+# specific commit, uncomment below variable
 #commit="19bb4c78bc"
+
+# Uncomment to use a different git remote:
 #remote="origin"
