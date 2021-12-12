c32c870eb112ec575e36130d5a4823b2676b356e
Cerem Cem ASLAN <cem@aktos.io>
2021-12-12 00:49:51 +0000

    improved ui: added statistics output after git fetch

--- latest-build-Version.h
+++ latest-build-Version.h
@@ -4,11 +4,11 @@
 #define FCVersionMinor "20"
 #define FCVersionName  "Vulcan"
 // test: $Format:Hash (%H), Date: %ci$
-#define FCRevision      "30593 (Git)"      //Highest committed revision number
-#define FCRevisionDate  "2021/11/20 22:35:21"     //Date of highest committed revision
+#define FCRevision      "30668 (Git)"      //Highest committed revision number
+#define FCRevisionDate  "2021/12/10 14:26:43"     //Date of highest committed revision
 #define FCRepositoryURL "Unknown"      //Repository URL of the working copy
 
 // Git relevant stuff
-#define FCRepositoryHash   "4b40f07a7b8d6bffff426bff8181287db5de9a35"
+#define FCRepositoryHash   "7edee4b30be4e74c2d38b560f19caf52ba114e8f"
 #define FCRepositoryBranch "LinkDaily"
 
