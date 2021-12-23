2c7a21e96d1071e8f4cbe1e907efa042e935341a
Cerem Cem ASLAN <cem@aktos.io>
2021-12-23 14:43:19 +0300

    fixed status message

--- tools/update-builder.sh
+++ tools/update-builder.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 _sdir=$(dirname "$(readlink -f "$0")")
+set -eu
 
 echo "Updating builder scripts inside the container."
-$_sdir/attach.sh "cd build-freecad-asm3; git fetch && git reset --hard origin/master"
-echo "Done."
+$_sdir/attach.sh "cd build-freecad-asm3; git fetch && git reset --hard origin/master && echo "Done." || echo "Failed.""
